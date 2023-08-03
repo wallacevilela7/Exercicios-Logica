2b12ca035fbed15e5237cbb3d138a1fe18751f59
Wallace Vilela <wallace.vilela9@gmail.com>
2023-08-03 18:37:20 -0300

    Exercicio 2 - resolução

--- Exercicio2/Program.cs
+++ Exercicio2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Faça um porograma para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo
+//com quatro casas decimais conforme exemplo.
+
+using System;
+using System.Globalization;
+
+namespace Ex {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor do raio: ");
+            double r = double.Parse(Console.ReadLine());
+
+            double area = Math.PI * Math.Pow(r, 2);
+            Console.WriteLine($"Area do círculo: {area.ToString("F4", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
