9ce2f56e8b2808e0d557c572ead7782b4ac878f3
Wallace Vilela <wallace.vilela9@gmail.com>
2023-08-03 18:25:27 -0300

    Exercicio 1 - resolvido

--- Exercicio1/Program.cs
+++ Exercicio1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Faça um exercicio para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma mensagem explicativa.
+
+using System;
+
+namespace Aula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira dois numeros inteiros");
+            Console.Write("Primeiro: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Segundo: ");
+            int b = int.Parse(Console.ReadLine());
+
+            int soma = a + b;
+            Console.WriteLine("Soma: " + soma);
+        }
+    }
+}
