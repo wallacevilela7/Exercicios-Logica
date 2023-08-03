ca1912a5c6681397f6906ab9e285b14119aa5b2e
Wallace Vilela <wallace.vilela9@gmail.com>
2023-08-03 18:44:02 -0300

    Exercicio 3

--- Exercicio3/Program.cs
+++ Exercicio3/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ * Fazer um programa para ler quatro valores inteiros A,B,C e D. A seguir, calcule e mostre a diferença
+ * do produto de A E B pelo produto de C e D.
+ */
+
+using System;
+using System.Globalization;
+
+namespace Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Valor de A:  ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Valor de B:  ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("Valor de C:  ");
+            int c = int.Parse(Console.ReadLine());
+            Console.Write("Valor de D:  ");
+            int d = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine();
+            int dif = (a * b) - (c * d);
+            Console.WriteLine($"Diferença de '(A * B) - (C * D)' é = {dif}");
+        }
+    }
+}
