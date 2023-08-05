65429f0577a648f50a2335a45e42ca33d7fd1f31
Wallace Vilela <wallace.vilela9@gmail.com>
2023-08-05 19:39:12 -0300

    Exercicio 6 - enunciado

--- Exercicio6/Program.cs
+++ Exercicio6/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ * Fazer um programa que leia três valores com ponto flutuante de dupla precisão a b e c. Em seguida, calcule e mostre:
+ * 1 - a area do circulo de raio c;
+ */
+
+using System;
