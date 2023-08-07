83921766588cad386fbea07fe761308429637a8f
Wallace Vilela <wallace.vilela9@gmail.com>
2023-08-07 15:01:29 -0300

    Exercicio 7 - resolucao

--- Exercicio6/Program.cs
+++ Exercicio6/Program.cs
@@ -39,6 +39,8 @@ namespace Ex
 
             double areaRetangulo = A * B;
             Console.WriteLine($"A area do Retângulo: {areaRetangulo.ToString("F2", CultureInfo.InvariantCulture)}");
+
+            
         }
     }
 }
---
+++ Exercicio7/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não.
+ */
+
+using System;
+using System.Threading.Channels;
+
+namespace Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número inteiro: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(VerificaPar(n));
+
+        }
+        static string VerificaPar(int x)
+        {
+            return x > 0 ? "É positivo" : "É negativo";
+        }
+    }
+}
