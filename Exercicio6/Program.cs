bb13373e6abfc9f17de56194034c3845be782ff7
Wallace Vilela <wallace.vilela9@gmail.com>
2023-08-07 11:12:20 -0300

    Exercicio 6 - finalizado

--- Exercicio6/Program.cs
+++ Exercicio6/Program.cs
@@ -1,6 +1,10 @@
 ﻿/*
  * Fazer um programa que leia três valores com ponto flutuante de dupla precisão a b e c. Em seguida, calcule e mostre:
  * 1 - a area do circulo de raio c;
+ * 2 - a área do triangulo retangulo que tem A por base C por altura;
+ * 3 - a área do trapézio que tem A e B por base e C por altura.
+ * 4 - a área do quadrado que tem lado B;
+ * 5 - a área do retângulo que tem lados A e B;
  */
 
 using System;
@@ -22,7 +26,19 @@ namespace Ex
 
 
             double areaCirculo = Math.PI * Math.Pow(C, 2);
-            Console.WriteLine($"A area do círculo com raio c é: {areaCirculo.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"A area do Círculo: {areaCirculo.ToString("F2", CultureInfo.InvariantCulture)}");
+
+            double areaTriangulo = (A * C)/2;
+            Console.WriteLine($"A area do Triângulo: {areaTriangulo.ToString("F2", CultureInfo.InvariantCulture)}");
+            
+            double areaTrapezio = ((A + B)*C) / 2;
+            Console.WriteLine($"A area do Trapézio: {areaTrapezio.ToString("F2", CultureInfo.InvariantCulture)}");
+
+            double areaQuadrado = B * B;
+            Console.WriteLine($"A area do Quadrado: {areaQuadrado.ToString("F2", CultureInfo.InvariantCulture)}");
+
+            double areaRetangulo = A * B;
+            Console.WriteLine($"A area do Retângulo: {areaRetangulo.ToString("F2", CultureInfo.InvariantCulture)}");
         }
     }
 }
