e2dad8254e0c587675ce22944ec90b0ab0192309
Wallace Vilela <wallace.vilela9@gmail.com>
2023-08-07 15:06:22 -0300

    Exercicio 8

--- Exercicio8/Program.cs
+++ Exercicio8/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ * Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar
+ */
+
+using System;
+
+namespace Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número inteiro: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(ParOuImpar(n));
+        }
+        static string ParOuImpar(int x)
+        {
+            return x % 2 == 0 ? "É par!" : "É impar!";
+        }
+    }
+}
