7526356cfab80b04fdc27777d84b85754eeb675e
Wallace Vilela <wallace.vilela9@gmail.com>
2023-08-07 16:05:16 -0300

    Exercicio 9 - resolução

---
+++ Exercicio9/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+ *  Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao 
+    Multiplos", indicando se os valores lidos são múltiplos entre si. Atenção: os números devem poder ser digitados em 
+    ordem crescente ou decrescente.
+ */
+
+using System;
+
+namespace Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número inteiro A: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite um número inteiro B: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.Clear();
+            Console.WriteLine($"A = {a}\nB = {b}\n"+ VerifyMutiplicity(a,b));
+        }
+        static string VerifyMutiplicity(int x, int y)
+        {
+            if(x % y == 0 || y % x == 0)
+            {
+                return "São múltiplos";
+            }
+            return "Não são múltiplos";
+        }
+    }
+}
