2ac7b574fa3ab24b8e9bf0b2fe8116db534dc5b7
Wallace Vilela <wallace.vilela9@gmail.com>
2023-08-08 16:35:09 -0300

    Exercicio 12 -  resolução

---
+++ Exercicio12/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Fazer um programa para ler três números inteiros e mostrar na tela o maior deles.
+ */
+
+
+using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite três números");
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Maior = {FindBigger(a, b, c)}");
+        }
+        static int FindBigger(int x, int y, int z)
+        {
+            int bigger = 0;
+
+            if(x > y && x > z)
+            {
+                bigger = x;
+            }
+            else if(y > x && y > z)
+            {
+                bigger = y;
+            }
+            else if(z > x && z > y)
+            {
+                bigger = z;
+            }
+
+
+        return bigger;
+        }
+    }
+}
