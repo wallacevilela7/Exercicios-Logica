94a0d27779b9c14d17003b0d4ad56541eccb8cc2
Wallace Vilela <wallace.vilela9@gmail.com>
2023-08-09 16:08:01 -0300

    Exercicio 15 - resolucao

---
+++ Exercicio15/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Escreva um programa que repita a leitura de ums senha até que ela seja válida. Para cada leitura de senha incorreta informada, escrever
+ * a mensagem "Senha Inválida". Quando a senha for informada corretamente deve ser impressa a mengagem "Acesso Permitido" e o algotitmo
+ * encerrado. Considere que a senha correta é o valor 2002.
+ */
+
+using System;
+
+namespace Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a senha");
+            string password = Console.ReadLine();
+
+            while (password != "2002")
+            {
+                Console.WriteLine("Senha Inválida!");
+                password = Console.ReadLine();
+            }
+            Console.Clear();
+            Console.WriteLine("Acesso permitido");
+
+        }
+    }
+}
