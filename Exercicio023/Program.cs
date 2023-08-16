d1d62d8032de3e2fcb0d63ffaee8b182558d8d35
Wallace Vilela <wallace.vilela9@gmail.com>
2023-08-16 15:27:42 -0300

    Exercicio 23 - resolução

---
+++ Exercicio023/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um valor inteiro: ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.Clear();
+            Console.WriteLine($"Os divisores de {x}: ");
+            for (int i = 1; i <= x; i++)
+            {
+                if(x % i == 0 )
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+}
