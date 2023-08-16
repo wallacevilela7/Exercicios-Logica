128f084f0857e11d38bcf962b3b354b4cc0d5fbf
Wallace Vilela <wallace.vilela9@gmail.com>
2023-08-16 15:20:05 -0300

    Exercicio 22 - resolucao

---
+++ Exercicio022/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Ler um valor N. Calcular e escrever seu respectivo fatorial. Fatorial de N = N * (N-1) * (N-2) * (N-3) * ... * 1.
+   Lembrando que, por definição, fatorial de 0 é 1.
+ */
+using System;
+
+namespace Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um valor inteiro: ");
+            int n = int.Parse(Console.ReadLine());
+            int ft = 1;
+
+            for(int i = 1; i <= n; i++)
+            {
+                ft = ft * i;
+            }
+            Console.WriteLine(ft);
+        }
+    }
+}
