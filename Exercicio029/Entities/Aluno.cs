ce9eca02bbbf45b171c35fc5a494b9aceca24ac9
Wallace Vilela <wallace.vilela9@gmail.com>
2023-08-18 17:47:19 -0300

    Exercicio 29 - resolucao

---
+++ Exercicio029/Entities/Aluno.cs
@@ -0,0 +1,38 @@
+﻿
+namespace Exercicio029.Entities
+{
+    internal class Aluno
+    {
+        public String Nome { get; set; }
+        public double Nota1 { get; set; }
+        public double Nota2 { get; set; }
+        public double Nota3 { get; set; }
+        public double Media { get; set; }
+
+        public Aluno(string nome, double nota1, double nota2, double nota3)
+        {
+            Nome = nome;
+            Nota1 = nota1;
+            Nota2 = nota2;
+            Nota3 = nota3;
+        }
+
+        public double CalculaMedia()
+        {
+            Media = Nota1 + Nota2 + Nota3;
+            return Media;
+        }
+        public string VerificaAprovacao()
+        {
+            if(Media >= 60)
+            {
+                return "Aprovado";
+            }
+            else
+            {
+                double faltou = 60.00 - Media;
+                return $"Reprovado\nFaltaram {faltou} pontos";
+            }
+        }
+    }
+}
---
+++ Exercicio029/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ Fazer um programa para ler o nome de uma aluno e as três notas que ele obteve nos três trimestres do ano (primeiro semestre vale 30 e o segundo e o terceiro valem 35 cada). Ao final, mostrar qual a nota final do aluno no ano. Dizer também se o aluno está aprovadi ou reprovado e, em caso negativo, quantos pontos faltam para o aluno obter o mínomo para ser aprovado que é 60 pontos. Voce deve criar a classe aluno para resolver este problema.
+ */
+
+using Exercicio029.Entities;
+
+namespace Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Nota 1: ");
+            double n1 = double.Parse(Console.ReadLine());
+            Console.Write("Nota 2: ");
+            double n2 = double.Parse(Console.ReadLine());
+            Console.Write("Nota 3: ");
+            double n3 = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Aluno aluno = new Aluno(nome, n1, n2, n3);
+
+            Console.WriteLine($"Media : {aluno.CalculaMedia()}");
+            Console.WriteLine(aluno.VerificaAprovacao());
+        }
+    }
+}
