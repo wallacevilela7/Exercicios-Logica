3ec2b747c63eb320d8ce7b4a691286b3a6c23df5
Wallace Vilela <wallace.vilela9@gmail.com>
2023-08-20 20:01:09 -0300

    Exercicio 30 - resolucao

---
+++ Exercicio030/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ Fazer um programa para ler um valor numérico qualquer, e daí mostrar quanto seria o valor de uma circunferência e do
+    volume de uma esfera para um raio daquele valor. Informar também o valor de Pi com duas casas decimais.
+ */
+
+using Exercicio030;
+using System.Globalization;
+
+namespace Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Calculadora.Circunferencia(raio);
+            double vol = Calculadora.Volume(raio);
+
+            Console.WriteLine($"Circunferência: {circ.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Volume: {vol.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Pi: {Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
