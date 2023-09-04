0112f1c923bc6b7ca66b23f4011f0bb80fb00f6e
Wallace Vilela <wallace.vilela9@gmail.com>
2023-09-04 19:30:48 -0300

    Exercicio 33

---
+++ Exercicio033/Entities/Client.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercicio033.Entities
+{
+    public class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Room { get; set; }
+
+        public Client()
+        {
+        }
+
+        public Client(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+        public Client(string name, string email, int room) : this(name, email)
+        {
+            Room = room;
+        }
+    }
+}
---
+++ Exercicio033/Program.cs
@@ -0,0 +1,27 @@
+﻿using Exercicio033.Entities;
+using System;
+
+namespace Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos quartos serão alugados? ");
+            int rented = int.Parse(Console.ReadLine());
+
+            Client[] clients = new Client[10];
+
+            for(int i = 0; i < rented; i++)
+            {
+                Console.WriteLine($"Rent #{i+1}:");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Room: ");
+                int room = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
