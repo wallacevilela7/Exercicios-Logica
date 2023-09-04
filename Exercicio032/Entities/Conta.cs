d15c9a6d751a54597e2f3d42420d18294a6f1b6a
Wallace Vilela <wallace.vilela9@gmail.com>
2023-09-04 19:30:29 -0300

    Exercicio 32

---
+++ Exercicio032/Entities/Conta.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+using System.Linq.Expressions;
+
+namespace Exercicio032.Entities
+{
+    public class Conta
+    {
+        private int Id { get; set; }
+        public string Nome { get; set; }
+        private double Saldo { get; set; }
+
+        public Conta(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+        public Conta(int id, string nome, double depositoInicial) : this(id, nome)
+        {
+            Deposito(depositoInicial);
+        }
+
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+        public bool Saque(double quantia)
+        {
+            if(quantia > (Saldo - 5.00))
+            {
+                return false;
+            }
+            else
+            {
+                Saldo -= quantia + 5.00;
+                return true;
+            }
+        }
+        public override string ToString()
+        {
+            return "Titular: "
+                + Nome
+                + ", "
+                + " R$: "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Exercicio032/Program.cs
@@ -0,0 +1,55 @@
+﻿using Exercicio032.Entities;
+using System.Globalization;
+
+namespace Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite os dados da conta:");
+            Console.WriteLine();
+            Console.Write("Numero da conta: ");
+            int id = int.Parse(Console.ReadLine()); 
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Deseja realizar um depósito inicial? (S/N)");
+            char option = char.Parse(Console.ReadLine());
+            double valorInicial = 0.00;
+
+            Conta conta;
+
+            if (option == 'S' || option == 's')
+            {
+                Console.Write("Digite o valor do depósito: R$");
+                valorInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new Conta(id, nome);
+                conta.Deposito(valorInicial);
+            }
+            else
+            {
+                conta = new Conta(id, nome);
+            }
+
+            Console.WriteLine($"Dados atualizados da conta: {conta}");
+
+            Console.Write("Valor para deposito: ");
+            double qtd = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(qtd);
+            Console.WriteLine($"Dados atualizados da conta: {conta}");
+
+
+            Console.Write("Valor para saque: ");
+            qtd = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            bool saque = conta.Saque(qtd);
+
+            Console.WriteLine(VerificaSaque(saque));
+
+            Console.WriteLine($"Dados atualizados da conta: {conta}");
+        }
+        static string VerificaSaque(bool saque)
+        {
+            return saque ? "Saque realizado com sucesso!" : "Saldo insuficiente";
+        }
+    }
+}
