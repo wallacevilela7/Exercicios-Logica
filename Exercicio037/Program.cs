d6dc9c56e5c6e9e5c2a6fdb8c9d37dd7356d43d4
Wallace Vilela <wallace.vilela9@gmail.com>
2023-09-10 20:23:37 -0300

    Exercicio 37 - resolução

--- Exercicio037/Program.cs
+++ Exercicio037/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.Design;
 
 namespace Ex
 {
@@ -6,12 +7,43 @@ namespace Ex
     {
         static void Main(string[] args)
         {
-            int x = 0, y = 0;
+            int x = int.MinValue; int y = int.MaxValue;
 
-
-            while(x != 0 || y != 0)
+            while (x != 0 || y != 0)
             {
+                x = int.Parse(Console.ReadLine());
+                y = int.Parse(Console.ReadLine());
+                Console.WriteLine(VerificaQuadrate(x,y));
 
+            }
+
+        }
+        static string VerificaQuadrate(int x, int y)
+        {
+            if (x == 0 || y == 0)
+            {
+                return null;
+            }
+            else if (x > 0 && y > 0)
+            {
+                return "Primeiro";
+            }
+            else if (x > 0 && y < 0)
+            {
+                return "Quarto";
+            }
+            else if (x < 0 && y < 0)
+            {
+                return "Terceiro";
+            }
+            else if (x < 0 && y > 0)
+            {
+                return "Segundo";
+
+            }
+            else
+            {
+                return "";
             }
         }
     }
