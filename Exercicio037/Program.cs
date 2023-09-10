07903bdeee7cc50586919a9a0ec2192034674f0c
Wallace Vilela <wallace.vilela9@gmail.com>
2023-09-10 20:13:37 -0300

    Exercicio 37

---
+++ Exercicio037/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0, y = 0;
+
+
+            while(x != 0 || y != 0)
+            {
+
+            }
+        }
+    }
+}
