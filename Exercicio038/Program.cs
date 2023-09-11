8d8f3f203f26f25976f79d7bf87c4fe2c8c53e0b
Wallace Vilela <wallace.vilela9@gmail.com>
2023-09-11 16:49:31 -0300

    Exercicio 38 - resolucao final

--- Exercicio038/Program.cs
+++ Exercicio038/Program.cs
@@ -10,21 +10,39 @@ namespace Ex
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
             int n = int.Parse(Console.ReadLine());
+
             int[,] mat = new int[n, n];
 
             for(int i = 0; i < n; i++)
             {
-                for(int j = 0; j < n; j++)
+                string[] values = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
                 {
-                    string[] values = Console.ReadLine().Split(' ');
-
                     mat[i, j] = int.Parse(values[j]);
                 }
             }
 
             Console.WriteLine("Diagonal principal: ");
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write(mat[i, i] + " ");
+            }
+            Console.WriteLine();
+
+
+            int count = 0;
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j <n; j++)
+                {
+                    if (mat[i,j] < 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine($"Quantidade de numeros negativos: {count}");
         }
     }
 }
