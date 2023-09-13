6fffb74f37d13a06e37536fc4afc12070d5d0239
Wallace Vilela <wallace.vilela9@gmail.com>
2023-09-13 20:02:41 -0300

    ExerciciO 40

---
+++ Exercicio040/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter com o primeiro valor (A)");
+            double a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Enter com o segundo valor (B)");
+            double b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine("Enter com o terceiro valor (C)");
+            double c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double delta = Math.Pow(b,2) - (4 * a * c);
+
+        }
+    }
+}
