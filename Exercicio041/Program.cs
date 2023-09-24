217531a54f14d2db198d083e452448abdf39a4fc
Wallace Vilela <wallace.vilela9@gmail.com>
2023-09-24 20:47:15 -0300

    Exercicio 41

---
+++ Exercicio041/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("VAI SÃO PAULO!!!! CAMPEÃO DA COPA DO BRASIL");
+        }
+    }
+}
