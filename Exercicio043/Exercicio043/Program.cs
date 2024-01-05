3c9051c2ee01b59eba17b751aa5bead7135fb4e5
Wallace Vilela <wallace.vilela9@gmail.com>
2024-01-05 18:36:08 -0300

    Exercico 43 - resolução

---
+++ Exercicio043/Exercicio043/Program.cs
@@ -0,0 +1,26 @@
+﻿using Exercicio043;
+using System;
+using System.Collections.Concurrent;
+
+namespace Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com os valores dos lados do retângulo");
+            Console.Write("Largura: ");
+            double width = double.Parse(Console.ReadLine());
+            Console.Write("Altura: ");
+            double height = double.Parse(Console.ReadLine());
+
+            Retangulo rtg = new Retangulo(width, height);
+
+            Console.WriteLine("");
+            Console.WriteLine("Dados do Retangulo: ");
+            Console.WriteLine($"Área: {rtg.Area()}");
+            Console.WriteLine($"Perímetro: {rtg.Perimetro()}");
+            Console.WriteLine($"Diagonal: {rtg.Diagonal()}");
+        }
+    }
+}
---
+++ Exercicio043/Exercicio043/Retangulo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio043
+{
+    internal class Retangulo
+    {
+        public double Largura { get; set; }
+        public double Altura { get; set; }
+
+        public Retangulo(double largura, double altura)
+        {
+            Largura = largura;
+            Altura = altura;
+        }
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+        public double Perimetro()
+        {
+            return (2 * (Largura + Altura));
+        }
+        public double Diagonal()
+        {
+            return Math.Sqrt(Math.Pow(Largura, 2) +  Math.Pow(Altura, 2));
+        }
+    }
+}
