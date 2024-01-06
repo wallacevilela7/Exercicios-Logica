0f758abe88d02c21396df91551d9563a2ee9efc0
Wallace Vilela <wallace.vilela9@gmail.com>
2024-01-06 16:40:38 -0300

    Exercicio 44 - resolução

---
+++ Exercicio044/Exercicio044/Funcionario.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio044
+{
+    internal class Funcionario
+    {
+        public string Nome { get; set; }
+        public double Imposto { get; set; }
+        public double SalarioBruto { get; set; }
+
+        public Funcionario() { }
+
+        public Funcionario(string nome, double imposto, double salarioBruto)
+        {
+            Nome = nome;
+            Imposto = imposto;
+            SalarioBruto = salarioBruto;
+        }
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double pct)
+        {
+            SalarioBruto = SalarioBruto + (SalarioBruto * (pct/100));
+        }
+
+        public override string ToString()
+        {
+            return $"Funcionario: {Nome}, $ {SalarioLiquido():F2}";
+        }
+    }
+}
---
+++ Exercicio044/Exercicio044/Program.cs
@@ -0,0 +1,33 @@
+﻿using Exercicio044;
+using System;
+using System.Globalization;
+
+namespace Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com os dados do funcionario:");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Salário Bruto: ");
+            double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Imposto: ");
+            double imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Funcionario f = new Funcionario(nome, imposto, salario);
+
+            Console.WriteLine(f);
+
+            Console.WriteLine("Digite a porcentagem para aumentar o salário: ");
+            double pct = double.Parse(Console.ReadLine());
+
+            f.AumentarSalario(pct);
+
+            Console.WriteLine($"Dados Atualizados: {f}");
+        }
+    }
+}
