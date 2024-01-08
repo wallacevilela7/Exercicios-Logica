44711e18ce729f61e5d0ce0ea908c5b4bb46f65e
Wallace Vilela <wallace.vilela9@gmail.com>
2024-01-08 20:04:58 -0300

    Exercicio 46 - resolução (beecrowd 1041)

---
+++ Exercicio046/Exercicio046/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.Xml;
+
+//Beecrowd - 1041 Coordenadas de um ponto
+
+namespace Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string l1 = Console.ReadLine();
+            string[] values = l1.Split(' ');
+            double x = double.Parse(values[0]);
+            double y = double.Parse(values[1]);
+
+            if(x == 0 && y == 0)
+                Console.WriteLine("Origem");
+            else if (x > 0 && y > 0)
+                Console.WriteLine("Q1");
+            else if (x < 0 && y < 0)
+                Console.WriteLine("Q3");
+            else if (x > 0 && y < 0)
+                Console.WriteLine("Q4");
+            else if (x < 0 && y > 0)
+                Console.WriteLine("Q2");
+            else if (x != 0 && y == 0)
+                Console.WriteLine("Eixo X");
+            else if (x == 0 && y != 0)
+                Console.WriteLine("Eixo Y");
+        }
+    }
+}
