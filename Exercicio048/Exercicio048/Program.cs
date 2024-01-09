fee49e1f1ee2522f11c01a52ff83b3cf823b274c
Wallace Vilela <wallace.vilela9@gmail.com>
2024-01-09 19:54:48 -0300

    Exercicio 48 - resolução (beecrowd 1047 Tempo de jogo

---
+++ Exercicio048/Exercicio048/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Immutable;
+
+namespace Ex
+{
+    class URI
+    {
+        static void Main(string[] args)
+        {
+            string[] valuesInput = Console.ReadLine().Split(' ');
+
+            int v1 = int.Parse(valuesInput[0]);
+            int v2 = int.Parse(valuesInput[1]);
+
+
+            int duracao = (v2 - v1 + 24) % 24;
+
+            //olha que ternário chique, resolvendo o problema se a entrada for 0 0
+            duracao = (duracao == 0) ? 24 : duracao;
+            Console.WriteLine($"O JOGO DUROU {duracao} HORA(S)");
+
+        }
+    }
+}
