8f6d4cfc202b0b1c14737814878834d6fbf54399
Wallace Vilela <wallace.vilela9@gmail.com>
2024-01-12 19:35:45 -0300

    Exercicio 53 - resolução

---
+++ Exercicio053/Exercicio053/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= n; i++)
+            {
+                if(i % 2 == 0 )
+                    Console.WriteLine($"{i}^2 = {Math.Pow(i, 2)}");
+            }
+
+        }
+    }
+}
