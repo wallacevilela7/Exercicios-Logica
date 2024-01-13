7f17088d847d0cf17bde6a7ff939f08742db8fd8
Wallace Vilela <wallace.vilela9@gmail.com>
2024-01-13 10:09:18 -0300

    Exercicio 55 - resolução (beecrowd 1097 Sequencia IJ 3)

---
+++ Exercicio055/Exercicio055/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int loopControl = 0;
+            int aux = 7;
+
+            for (int i = 1; i <= 9; i += 2)
+            {
+                for (int j = aux; j <= 15; j--)
+                {
+                    loopControl++;
+                    Console.WriteLine($"I={i} J={j}");
+                    if(loopControl == 3) { loopControl = 0; aux += 2; break; }
+                }
+            }
+
+        }
+    }
+}
