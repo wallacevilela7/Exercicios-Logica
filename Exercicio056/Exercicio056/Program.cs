ba28c9385eb92d2cf6313ad3d5dd0b96cd282afa
Wallace Vilela <wallace.vilela9@gmail.com>
2024-01-14 10:04:58 -0300

    Exercicio 56 - resolucao (beecrowd 1099 Soma de impares consecutivos 2)

---
+++ Exercicio056/Exercicio056/Program.cs
@@ -0,0 +1,35 @@
+﻿int numTestes = int.Parse(Console.ReadLine());
+int imparCount = 0;
+
+for (int i = 0; i < numTestes; i++)
+{
+    string[] valores = Console.ReadLine().Split(' ');
+
+    int x = int.Parse(valores[0]);
+    int y = int.Parse(valores[1]);
+
+    if (x > y)
+    {
+        for (int w = y + 1; w < x; w++)
+        {
+            if (w % 2 != 0)
+            {
+                imparCount += w;
+            }
+        }
+    }
+
+    if (y > x)
+    {
+        for (int w = x + 1; w < y; w++)
+        {
+            if (w % 2 != 0)
+            {
+                imparCount += w;
+            }
+        }
+    }
+
+    Console.WriteLine(imparCount);
+    imparCount = 0;
+}
