e542821d35d24472da3276c56a89c8e30fa26563
Wallace Vilela <wallace.vilela9@gmail.com>
2024-01-17 16:53:21 -0300

    Exercicio 68 - 69 (beecrowd)

---
+++ Exercicio068/Exercicio068/Program.cs
@@ -0,0 +1,23 @@
+﻿
+string[] vet = Console.ReadLine().Split(' ');
+
+int a;
+int n;
+int soma = 0;
+
+a = int.Parse(vet[0]);
+n = int.Parse(vet[1]);
+
+int contv = 1;
+while (n <= 0)
+{
+    contv++;
+    n = int.Parse(vet[contv]);
+}
+
+for (int i = 0; i <= n - 1; i++)
+{
+    soma = soma + (a + i);
+
+}
+Console.WriteLine(soma);
---
+++ Exercicio069/Exercicio069/Program.cs
@@ -0,0 +1,27 @@
+﻿int num = int.Parse(Console.ReadLine());
+int soma = 0;
+int aux = 0;
+
+if(num <= 0 || num >= 46)
+{
+    do
+    {
+        num = int.Parse(Console.ReadLine());
+    } while (num <= 0 || num >= 46);
+}
+
+for(int i = 0; i <= num; i++)
+{
+    //Como pegar os dois ultimos i e apresentar a soma deles ??????????????????????????
+    if(i == 0 || i == 1)
+    {
+        Console.Write($"{i} ");
+    }
+    else if(i > 1)
+    {
+        aux = i - 1;
+        soma = (soma + aux);
+        Console.Write($"{soma} ");
+    }
+    
+}
