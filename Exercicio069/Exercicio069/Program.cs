fce4a36c0c807b29953695b37a829c78059ac342
Wallace Vilela <wallace.vilela9@gmail.com>
2024-01-17 18:16:48 -0300

    Exercicio 69 resolvido(PYTHON) (beecrowd 1151 - Fibonacci)

--- Exercicio069/Exercicio069/Program.cs
+++ Exercicio069/Exercicio069/Program.cs
@@ -36,7 +36,30 @@ foreach (int i in array)
     }
     else
     {
-        Console.Write($"{i}...");
+        Console.Write(i);
     }
 }
 Console.WriteLine();
+
+
+/*
+ * 
+ * no beecrowd só passou com python
+ * 
+ * N  = int(input())
+
+V1 = [0]*N
+
+for i in range (0,N):
+    if i <= 1:
+        V1[i] = i
+    else:
+        V1[i] = V1[i-1] + V1[i-2]
+
+    if i==N-1:
+        print('%d'%(V1[i]),end='')
+    else:
+        print('%d'%(V1[i]),end=' ')
+    
+print() #Quebra de linha 
+ */
