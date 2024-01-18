35f34d5527ea0c3c31b24a093737c3ffdbbd7ce4
Wallace Vilela <wallace.vilela9@gmail.com>
2024-01-18 17:18:56 -0300

    Exercicio 80 (beecrowd 1172 - Substituição em Vetor 1)

---
+++ Exercicio080/Exercicio080/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] vet = new int[10];
+
+for (int i = 0;i <10; i++)
+{
+    vet[i] = int.Parse(Console.ReadLine());
+}
+
+for(int i = 0;i < 10; i++)
+{
+    if (vet[i] <= 0) vet[i] = 1;
+}
+
+for(int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"X[{i}] = {vet[i]}");
+}
