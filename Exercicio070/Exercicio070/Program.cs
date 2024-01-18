7c18225bb4bf167202b958fe5f6cd20ed4f2bb70
Wallace Vilela <wallace.vilela9@gmail.com>
2024-01-17 21:36:06 -0300

    Exercicio 70 (beecrowd 1153 Fatorial)

---
+++ Exercicio070/Exercicio070/Program.cs
@@ -0,0 +1,23 @@
+﻿int num = int.Parse(Console.ReadLine());
+int fat = 1;
+
+if(num < 0 || num > 13)
+{
+    do
+    {
+        num = int.Parse(Console.ReadLine());
+    } while (num < 0 || num > 13);
+}
+
+
+for(int i = 1; i <= num; i++)
+{
+    fat += fat * (num - i);
+}
+
+Console.WriteLine(fat);
+
+//fatorial de 5 
+
+//5! = 5 * 4; * 3 * 2 * 1 = 120;
+
