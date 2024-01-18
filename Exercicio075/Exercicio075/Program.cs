ff11cabbfdc35d069268964a8f20b491b16d0540
Wallace Vilela <wallace.vilela9@gmail.com>
2024-01-18 11:48:19 -0300

    Exercicio 75 (beecrowd 1158 - Soma de ímpares consecutivos 3

---
+++ Exercicio075/Exercicio075/Program.cs
@@ -0,0 +1,20 @@
+﻿int num = int.Parse(Console.ReadLine());
+int soma = 0;
+
+for (int i = 0; i < num; i++)
+{
+    string[] input = Console.ReadLine().Split(' ');
+    int x = int.Parse(input[0]);
+    int y = int.Parse(input[1]);
+
+    for(int j = 1; j <= y * 2; j++)
+    {
+        if(x % 2 != 0)
+        {
+            soma += x;
+        }
+        x++;
+    }
+    Console.WriteLine(soma);
+    soma = 0;
+}
