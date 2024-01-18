7b2c0964b3dd1cb309fb653b74e37118dc96e621
Wallace Vilela <wallace.vilela9@gmail.com>
2024-01-18 15:45:00 -0300

    Exercicio 77 (beecrowd 1160 Crescimento Populacional)

---
+++ Exercicio077/Exercicio077/Program.cs
@@ -0,0 +1,36 @@
+﻿int x = int.Parse(Console.ReadLine());
+
+int pa, pb;
+double txA, txB;
+int anos = 0;
+
+for (int i = 0; i < x; i++)
+{
+    string[] input = Console.ReadLine().Split(' ');
+    pa = int.Parse(input[0]);
+    pb = int.Parse(input[1]);
+    txA = double.Parse(input[2]);
+    txB = double.Parse(input[3]);
+
+    txA = txA / 100;
+    txB = txB / 100;
+
+    do
+    {
+        pa += (int)(pa * txA);
+        pb += (int)(pb * txB);
+        anos++;
+
+    } while (pa <= pb);
+    if (anos > 100)
+    {
+        Console.WriteLine("Mais de 1 seculo.");
+    }
+    else
+    {
+        Console.WriteLine($"{anos} anos.");
+    }
+
+    input = new string[0];
+    anos = 0;
+}
