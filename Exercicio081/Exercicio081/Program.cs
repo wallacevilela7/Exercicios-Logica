1ed91e5e0d53106262e41209b7dc0cd2d4cbbc75
Wallace Vilela <wallace.vilela9@gmail.com>
2024-01-18 17:28:50 -0300

    Exercicio 81 (beecrowd 1173 - Preenchimento de Vetor 1)

---
+++ Exercicio081/Exercicio081/Program.cs
@@ -0,0 +1,14 @@
+﻿int x = int.Parse(Console.ReadLine());
+
+int[] vect = new int[10];
+
+for (int i = 0; i < 10; i++)
+{
+    vect[i] = x;
+    x *= 2;
+}
+
+for(int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"N[{i}] = {vect[i]}");
+}
