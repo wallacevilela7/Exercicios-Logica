1532b331f3492a585dd5347c44973dba0cda3d3c
Wallace Vilela <wallace.vilela9@gmail.com>
2024-01-18 17:11:43 -0300

    Exercicio 78 (beecrowd 1164 Numero perfeito)

---
+++ Exercicio078/Exercicio078/Program.cs
@@ -0,0 +1,28 @@
+﻿int x = int.Parse(Console.ReadLine());
+int soma = 0;
+int num;
+int i = 0;
+do
+{
+    num = int.Parse(Console.ReadLine());
+    for (int j = 1; j < num; j++)
+    {
+        if (num % j == 0)
+        {
+            soma += j;
+        }
+    }
+
+    if(soma == num)
+    {
+        Console.WriteLine($"{num} eh perfeito");
+    }else
+    {
+        Console.WriteLine($"{num} nao eh perfeito");
+    }
+
+    soma = 0;
+    i++;
+
+}while (i < x);
+        
