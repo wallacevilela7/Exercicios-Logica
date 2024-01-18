a9085418a3b1acb6b38912f7e1745cfa7f2f6227
Wallace Vilela <wallace.vilela9@gmail.com>
2024-01-18 17:12:12 -0300

    Exercicio 79 (beecrowd 1165 Numero Primo)

---
+++ Exercicio079/Exercicio079/Program.cs
@@ -0,0 +1,19 @@
+﻿int x = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < x; i++) {
+    int num = int.Parse(Console.ReadLine());
+    string output = ehPrimo(num);
+    Console.WriteLine(output);
+}
+
+static string ehPrimo(int num)
+{
+    int divisores = 0;
+    for (int j = 1; j < num; j++)
+    {
+        if (num % j == 0) divisores++;
+    }
+
+    return divisores > 1 ? $"{num} nao eh primo" : $"{num} eh primo";
+
+}
