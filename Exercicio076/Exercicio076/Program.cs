afc7810407058e1d05c0daeccd5b60e034e4f656
Wallace Vilela <wallace.vilela9@gmail.com>
2024-01-18 15:45:43 -0300

    Exercicio 76 (beecrowd 1159 Soma de pares consecutivos )

---
+++ Exercicio076/Exercicio076/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml.Serialization;
+
+int x = int.Parse(Console.ReadLine());
+int soma = 0;
+int count = x;
+
+while (x != 0)
+{
+    for (int i = x; i <= count + 9; i++)
+    {
+        if (x % 2 == 0)
+        {
+            soma += x;
+        }
+        x++;
+    }
+    Console.WriteLine(soma);
+    soma = 0;
+    x = int.Parse(Console.ReadLine());
+    count = x;
+}
