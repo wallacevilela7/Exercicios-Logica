af2e5bd58a255cf49ffb59549eb8aa42d3d9bf08
Wallace Vilela <wallace.vilela9@gmail.com>
2024-01-19 15:31:55 -0300

    Exercicio 84 beecrowd 1176 Fibonacci em vetor

---
+++ Exercicio084/Exercicio084/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Buffers;
+
+long[] serieFibonacci = new long[61];
+
+for(int i = 0; i < 61; i++)
+{
+    if (i == 0 || i == 1)
+        serieFibonacci[i] = i;
+    else
+    {
+        serieFibonacci[i] = (serieFibonacci[i - 1] + serieFibonacci[i - 2]);
+    }
+}
+
+int x = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < x; i++)
+{
+    int index = int.Parse(Console.ReadLine());
+    Console.WriteLine($"Fib({index}) = {serieFibonacci[index]}");
+}
+
