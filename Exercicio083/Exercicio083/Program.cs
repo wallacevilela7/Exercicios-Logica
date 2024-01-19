6a0fa1a07ffcc2fa0c78e23d73b681632a1ae778
Wallace Vilela <wallace.vilela9@gmail.com>
2024-01-19 11:52:52 -0300

    Exercicio 83 - beecrowd 1175 Troca em vetor 1

---
+++ Exercicio083/Exercicio083/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+
+int[] vet = new int[20];
+int[] vet2 = new int[20];
+int j = 0;
+
+for (int i = 0; i < 20; i++)
+{
+    vet[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 19; i >= 0; i--)
+{
+    vet2[j] = vet[i];
+    j++;
+}
+
+for (int k = 0; k < 20; k++)
+{
+    Console.WriteLine($"N[{k}] = {vet2[k]}");
+}
+
+
+
