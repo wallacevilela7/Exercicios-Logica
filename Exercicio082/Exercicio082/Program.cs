44c50f46b82874ddc6765602838f731a64c5dad5
Wallace Vilela <wallace.vilela9@gmail.com>
2024-01-19 11:43:02 -0300

    Exercicio 82 - beecrowd 1174 Seleção em vetor 1

---
+++ Exercicio082/Exercicio082/Program.cs
@@ -0,0 +1,14 @@
+﻿double[] vect = new double[100];
+
+for (int i = 0; i < 100; i++)
+{
+    vect[i] = double.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < 100; i++)
+{
+    if (vect[i] <= 10.0)
+    {
+        Console.WriteLine($"A[{i}] = {vect[i]:F1}");
+    }
+}
