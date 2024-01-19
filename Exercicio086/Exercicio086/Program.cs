d7d40ea20a14a23c69118c53377278c83090c3f9
Wallace Vilela <wallace.vilela9@gmail.com>
2024-01-19 15:32:53 -0300

    Exercicio 86 beecrowd 1178 Preenchimento em vetor 3

---
+++ Exercicio086/Exercicio086/Program.cs
@@ -0,0 +1,40 @@
+﻿decimal x = decimal.Parse(Console.ReadLine());
+
+decimal[] vet = new decimal[100];
+
+vet[0] = x;
+
+for (int i = 1; i < 100; i++)
+{
+    x = x / 2;
+    vet[i] = x;
+}
+
+for (int i = 0; i < 100; i++)
+{
+    Console.WriteLine($"N[{i}] = {vet[i]:f4}");
+}
+
+/*Passou mas foi em c 
+ * #include <stdio.h>
+
+int main() {
+    double x;
+    scanf("%lf", &x);
+
+    double vet[100];
+
+    vet[0] = x;
+
+    for (int i = 1; i < 100; i++) {
+        x = x / 2;
+        vet[i] = x;
+    }
+
+    for (int i = 0; i < 100; i++) {
+        printf("N[%d] = %.4lf\n", i, vet[i]);
+    }
+
+    return 0;
+}
+ */
