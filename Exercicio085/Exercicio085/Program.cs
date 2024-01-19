9e7c1bd38ea29c2e0fc47ecd5afcc3ad5a9e968f
Wallace Vilela <wallace.vilela9@gmail.com>
2024-01-19 15:32:28 -0300

    Exercicio 85 beecrowd 1177 Preenchimento em vetor 2

---
+++ Exercicio085/Exercicio085/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http.Headers;
+using System.Numerics;
+
+int[] vet = new int[1000];
+int x = int.Parse(Console.ReadLine());
+int aux = x - 1;
+int count = 0;
+if(!(x >= 2 && x <= 50))
+{
+    do
+    {
+        x = int.Parse(Console.ReadLine());
+    } while (!(x >= 2 && x <= 50));
+}
+
+for(int i = 0; i < 1000; i++)
+{
+    if(count <= aux)
+    {
+        vet[i] = count;
+    }
+    count++;
+    if (count > aux) count = 0;
+}
+
+for(int i = 0; i < 1000; i++)
+{
+    Console.WriteLine($"N[{i}] = {vet[i]}");
+}
