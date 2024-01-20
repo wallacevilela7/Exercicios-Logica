88394b756db1e67c4bb45fd8c1788322e2d36877
Wallace Vilela <wallace.vilela9@gmail.com>
2024-01-20 19:50:58 -0300

    Exercicio 89 - beecrowd 1181 Linha na Matriz

---
+++ Exercicio089/Exercicio089/Program.cs
@@ -0,0 +1,48 @@
+﻿try
+{
+
+
+int[,] mat = new int[12, 12];
+
+int linha = 0;
+linha = int.Parse(Console.ReadLine());
+string operacao = "";
+operacao = Console.ReadLine();
+int[] vet = new int[12];
+double saida = 0.0;
+
+for (int i = 0; i < 12; i++)
+{
+    for (int j = 0; j < 12; j++)
+    {
+        mat[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+
+
+for (int i = linha; i < 12; i++)
+{
+    for (int j = 0; j < 12; j++)
+    {
+        vet[j] = mat[i, j];
+    }
+}
+
+for (int i = 0; i < 12; i++)
+{
+    saida += vet[i];
+}
+
+if (operacao == "M" || operacao == "m")
+{
+    saida = saida / 12;
+}
+
+
+Console.WriteLine($"{saida:F1}");
+
+}
+catch(FormatException ex)
+{
+    Console.WriteLine(ex.Message);
+}
