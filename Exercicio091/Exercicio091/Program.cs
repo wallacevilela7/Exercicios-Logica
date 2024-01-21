fb0ec31829f5aac98b428a4ad0702f8b1b788328
Wallace Vilela <wallace.vilela9@gmail.com>
2024-01-21 18:09:39 -0300

    Exercicio 91 beecrowd 1183 Acima da diagonal principal

---
+++ Exercicio091/Exercicio091/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+char op;
+const int size = 12;
+double[,] mat = new double[size, size];
+List<double> vect = new List<double>();
+double output = 0.0;
+
+op = char.Parse(Console.ReadLine());
+
+for (int i = 0; i < size; i++)
+{
+    for (int j = 0; j < size; j++)
+    {
+        mat[i,j] = double.Parse(Console.ReadLine());
+    }
+}
+
+for (int i = 0; i < size; i++)
+{
+    for (int j = 0; j < size; j++)
+    {
+        if (j > i)
+        {
+            vect.Add(mat[i, j]);
+        }
+    }
+}
+
+if (op == 'S')
+{
+    output = vect.Sum();
+    Console.WriteLine($"{output:F1}");
+}
+else if(op == 'M')
+{
+    output = vect.Average();
+    Console.WriteLine($"{output:F1}");
+}
