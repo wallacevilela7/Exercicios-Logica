3174752d3567146fafc525484e38b333c7069d2e
Wallace Vilela <wallace.vilela9@gmail.com>
2024-01-22 09:51:46 -0300

    Exercicio 92 beecrowd 1184 Abaixo da diagonal principal

---
+++ Exercicio092/Exercicio092/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Numerics;
+using System.Runtime.Serialization;
+
+char op;
+int size = 12;
+double[,] mat = new double[size,size];
+List<double> vet = new List<double>();
+double output = 0.0;
+
+op = char.Parse(Console.ReadLine());
+
+for (int i = 0; i < size; i++)
+{
+    for (int j = 0; j < size; j++)
+    {
+        mat[i,j] = double.Parse(Console.ReadLine());
+        if(i > j) vet.Add(mat[i,j]);
+    }
+}
+
+output = vet.Sum();
+
+if (op == 'S')
+    Console.WriteLine($"{output:f1}");
+else if (op == 'M') output = vet.Average(); Console.WriteLine($"{output:f1}");
