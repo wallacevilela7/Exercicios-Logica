e00bbf254bdccdd33c78f8bcb733e1688d986488
Wallace Vilela <wallace.vilela9@gmail.com>
2024-01-22 15:02:40 -0300

    Exercicio 99 beecrowd 1478 Matriz quadrada 2

--- Exercicio099/Exercicio099/Program.cs
+++ Exercicio099/Exercicio099/Program.cs
@@ -90,3 +90,54 @@ int main()
 
     }
 }*/
+
+//////////////////MATRIZ QUADRADA 2
+/*
+ * #include <stdio.h>
+
+int main ()
+{
+
+	unsigned short int tamanho, linha, coluna;
+
+	while (1)
+	{
+
+		scanf("%hd", &tamanho);
+		short matriz[tamanho][tamanho];
+
+		if (tamanho == 0)
+			return 0;
+
+		for(linha = 0; linha < tamanho; linha++)
+			for(coluna = 0; coluna < tamanho; coluna++)
+			{
+				if (linha == coluna)
+					matriz[linha][coluna] = 1;
+				if (linha < coluna)
+					matriz[linha][coluna] = coluna - linha + 1;
+				if (linha > coluna)
+					matriz[linha][coluna] = linha - coluna + 1;
+			}
+
+
+		//Imprime a matriz;
+		for (linha = 0; linha < tamanho; linha++)
+		{
+			for (coluna = 0; coluna < tamanho; coluna++)
+			{
+				if (coluna == 0)
+					printf("%3hd", matriz[linha][coluna]);
+				else
+					printf(" %3hd", matriz[linha][coluna]);
+
+			}
+
+			printf("\n");
+		}
+
+		printf("\n");
+
+	}
+}
+ */
