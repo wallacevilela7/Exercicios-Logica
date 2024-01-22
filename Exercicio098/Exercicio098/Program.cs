8a91e73c929c05432fe37c17197c75286e2b99c7
Wallace Vilela <wallace.vilela9@gmail.com>
2024-01-22 14:13:22 -0300

    Exercicio 98 beecrowd 1190 Área direita

---
+++ Exercicio098/Exercicio098/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+char op;
+int size = 12;
+double[,] mat = new double[size, size];
+List<double> list = new List<double>();
+double output = 0.0;
+
+op = char.Parse(Console.ReadLine());
+
+for (int i = 0; i < size; i++)
+{
+    for (int j = 0; j < size; j++)
+    {
+        mat[i, j] = double.Parse(Console.ReadLine());
+        if (j > i && i + j > 11) list.Add(mat[i, j]);
+    }
+}
+
+if (op == 'S')
+{
+    output = list.Sum();
+    Console.WriteLine($"{output:F1}");
+}
+else if (op == 'M')
+{
+    output = list.Average();
+    Console.WriteLine($"{output:F1}");
+}
