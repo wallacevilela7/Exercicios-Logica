241ddee7458f1339a2fa27db978ec6aca3435972
Wallace Vilela <wallace.vilela9@gmail.com>
2024-01-24 18:24:48 -0300

    Exercicio 103 beecrowd 1589 Bob conduite

---
+++ Exercicio103/Exercicio103/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            string[] input = new string[0];
+            int rc, r1, r2;
+
+            x = int.Parse(Console.ReadLine());
+
+            int i = 0;
+            while(i < x)
+            {
+                input = Console.ReadLine().Split(' ');
+                r1 = int.Parse(input[0]);
+                r2 = int.Parse(input[1]);
+
+                rc = r1 + r2;
+
+                Console.WriteLine(rc);
+                i++;
+            }
+        }
+    }
+}
