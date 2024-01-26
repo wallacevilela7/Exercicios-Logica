7604cffcfb170fcf3d38bfb8439cacd8f176cab8
Wallace Vilela <wallace.vilela9@gmail.com>
2024-01-25 21:02:10 -0300

    Exercicio 105 - beecrowd 1789 Corrida de Lesmas

---
+++ Exercicio105/Exercicio105/Program.cs
@@ -0,0 +1,30 @@
+﻿
+while (true)
+{
+    if(!int.TryParse(Console.ReadLine(), out int qtdGrupo))
+    {
+        break;
+    }
+
+    int element = 0;
+    List<int> list = new List<int>();
+
+    string[] input = new string[qtdGrupo];
+    input = Console.ReadLine().Split(' ');
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        element = int.Parse(input[i]);
+        list.Add(element);
+    }
+
+    Console.WriteLine(GetBigger(list));
+}
+
+static int GetBigger(List<int> list)
+{
+    int bigger = list.Max(x => x);
+    if (bigger < 10) return 1;
+    else if (bigger >= 10 && bigger < 20) return 2;
+    else return 3;
+}
