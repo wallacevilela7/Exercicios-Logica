277bbc652b29224bbe9e40a314cb651efc57a15e
Wallace Vilela <wallace.vilela9@gmail.com>
2024-01-27 18:34:11 -0300

    Exercicio 108 beecrowd 1847 Bem vindos e bem vindas ao inverno

---
+++ Exercicio108/Exercicio108/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            int a, b, c;
+
+
+        }
+    }
+}
