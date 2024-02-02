c2a0df123eb6991293b17cf43086134eee8f1b05
Wallace Vilela <wallace.vilela9@gmail.com>
2024-02-02 20:58:24 -0300

    Exercicio 109 enunciado beecrowd 1848 Corvo Contador

--- Exercicio108/Exercicio108/Program.cs
+++ Exercicio108/Exercicio108/Program.cs
@@ -7,9 +7,30 @@ namespace Ex
         static void Main(string[] args)
         {
             Console.WriteLine("");
-            int a, b, c;
+            int dia1, dia2, dia3;
+            string[] input = new string[0];
+
+            input = Console.ReadLine().Split(' ');
+            dia1 = int.Parse(input[0]);
+            dia2 = int.Parse(input[1]);
+            dia3 = int.Parse(input[2]);
+
+            string emoji = ChecaCondicao(dia1, dia2, dia3);
+            Console.WriteLine(emoji);
+        }
+
+        static string ChecaCondicao(int d1, int d2, int d3)
+        {
+            if (d2 > d1)
+            {
+                if ((d2 <= d3))
+                {
+                    return ":)";
+                }
+            }
 
 
+            return "";
         }
     }
 }
