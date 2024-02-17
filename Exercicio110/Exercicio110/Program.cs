fdf20bd24aa9dfae6a2fc4671104fe3fdcf817e1
Wallace Vilela <wallace.vilela9@gmail.com>
2024-02-16 21:07:07 -0300

    Exercicio 110 beecrowd 1864 Nossos dias nunca voltarão

---
+++ Exercicio110/Exercicio110/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+String phrase = "LIFE IS NOT A PROBLEM TO BE SOLVED";
+char[] phraseArray = new char[phrase.Length];
+
+//copiar uma string para um array de char posição por posição
+for (int i = 0; i < phrase.Length; i++)
+{
+    phraseArray[i] = phrase[i];
+}
+int x = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < x; i++)
+{
+    if(i < x - 1)Console.Write(phraseArray[i]);
+    else Console.Write($"{phraseArray[i]}\n");
+}
