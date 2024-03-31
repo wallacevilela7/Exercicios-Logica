4cec5142d3946089b0b2223c4c12946468f38615
Wallace Vilela <wallace.vilela9@gmail.com>
2024-03-31 20:09:45 -0300

    Exercicio 111 - beecrowd 1541 Construindo casas

---
+++ Exercicio111/Exercicio11.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class URI 1541 {
+	
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        double A, B, C, X;
+        while ((A = leitor.nextInt()) != 0) {
+        	B = leitor.nextInt();
+        	C = leitor.nextInt();
+        	X = Math.sqrt((((A * B) / C) * 100.0));
+        	System.out.println((int)X);
+        }
+    }
+	
+}
